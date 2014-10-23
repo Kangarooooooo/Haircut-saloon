dd24be3338e7b5341631b4bf6187d1b384e19a05
jonasandersen <jonasandersen@10.16.168.238>
2014-10-23 11:43:37 +0200

    fgrdfdg

--- Saloon/src/dto/HistorieDTO.java
+++ Saloon/src/dto/HistorieDTO.java
@@ -7,9 +7,17 @@ public class HistorieDTO {
 	int Frisoer_id;
 	int Bruger_id;
 	
-	public HistorieDTO(int Bestilling_id, String Startid, int Frisoer_id, int Bruger_id)
+	public HistorieDTO(int Bestilling_id, String Starttid, int Frisoer_id, int Bruger_id)
 	{
-		
+		this.Bestilling_id = Bestilling_id;
+		this.Starttid = Starttid;
+		this.Frisoer_id = Frisoer_id;
+		this.Bruger_id = Bruger_id;
+	}
+	
+	public HistorieDTO()
+	{
+	
 	}
 
 }
