8bfa6f34604579400f65173232f3aec0aa412f5e
jonasandersen <jonasandersen@10.16.168.238>
2014-10-27 16:55:21 +0100

    yes yes

--- Saloon/src/dao/MySQLHistorieDAO.java
+++ Saloon/src/dao/MySQLHistorieDAO.java
@@ -2,14 +2,13 @@ package dao;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import connector.Connector;
-
 import daoInterfaces.DALException;
 import daoInterfaces.HistorieDAO;
+import dto.BrugerDTO;
 import dto.HistorieDTO;
 
 public class MySQLHistorieDAO implements HistorieDAO{
@@ -17,15 +16,37 @@ public class MySQLHistorieDAO implements HistorieDAO{
 	@Override
 	public HistorieDTO getHistorie(int Bestilling_id, int Frisoer_id,
 			int Bruger_id) throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		ResultSet rs = Connector.doQuery("SELECT * FROM Historie WHERE Bestilling_id = " + Bestilling_id + " AND Frisoer_id = "
+			+	Frisoer_id + " AND Bruger_id = " + Bruger_id);
+		try 
+		{
+			if(!rs.first()) throw new DALException("Historien eksisterer ikke");
+			return new HistorieDTO (rs.getInt("Bestilling_id"), rs.getString("Starttid"), rs.getInt("Frisoer_id"),  rs.getInt("Bruger_id"));
+		}
+		catch (SQLException e) 
+		{
+			throw new DALException(e);
+		}
 	}
 
 	@Override
 	public List<HistorieDTO> HistorieList(HistorieDTO historie)
 			throws DALException {
-		// TODO Auto-generated method stub
-		return null;
+		List<HistorieDTO> list = new ArrayList<HistorieDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM Historie");
+		try{
+			while(rs.next())
+			{
+				list.add(new HistorieDTO(rs.getInt("Bestilling_id"), rs.getString("Starttid"), rs.getInt("Frisoer_id"),  rs.getInt("Bruger_id")));
+			}
+		}
+		catch(SQLException e)
+		{
+			throw new DALException(e);
+		}
+		return list;
+		
+		
 	}
 
 	@Override
